d0d4476c1a3146509f431a373a57800a32586fa3
Kaiz78 <amine.78mok@gmail.com>
2024-03-01 10:45:57 +0100

    correction de la home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,10 @@
-import { Col, Container, Row } from "react-bootstrap";
 import styled from "styled-components";
-
 import { Link } from "react-router-dom";
-
 import { TypeAnimation } from 'react-type-animation';
-
-import ProjectCard from "../components/projectCard";
 import { useSelector } from "react-redux";
-
 import { selectData } from "../services/homeSlice";
+
+
 const StyledAboutMe = styled.section`
   p {
     font-size: 1.25rem;
@@ -33,54 +29,41 @@ const Title = styled.div`
 
 
 function Home()  {    
-    const roles = ["Pythonist", "Developer", "Fast Learner"];
   
 
-    const { avatar_url, bio, name } = useSelector(selectData);
-
-
-    console.log(avatar_url);
-   
+    const { avatar_url, name } = useSelector(selectData);
   
   return (
     <>
       <StyledAboutMe className="section">
-        <Container>
-          <Container className="d-flex">
+        <div className="container">
+          <div className="container d-flex">
             <Title>
               <h2>Accueil</h2>
               <div className="underline"></div>            
             </Title>
-          </Container>
+          </div>
           <div className="p-2 align-items-center mt-5 ">
-                <div className="card-home text-center">
-                                <div style={
-                                    {
-                                        width: '30%',
-                                        
-                                    }
-                                }>
+            <div className="card-home text-center">
+              <div>
+                <img
+                    src={avatar_url}
+                    alt="GitHub Avatar"
+                    loading="lazy"
+                    className="mx-auto rounded-circle float-left"
+                    style={{ width: "100%", height: "100%" }} />
+              </div>
+              <div className="" style={
+                  {
+                      width: "80%",
+                      
+                  }
+              }>
 
-<img
-    src={avatar_url}
-    alt="GitHub Avatar"
-    loading="lazy"
-    className="mx-auto rounded-circle float-left"
-    style={{ width: "100%", height: "100%" }}
-/>
-</div>
-<div className="" style={
-    {
-        width: "100%",
-        
-    }
-}>
-
-                    <p>Hi 👋,</p>
-                    <p>I'm {name}</p>
-    
-<p>
-    A <span>
+                <p>Hi 👋,</p>
+                <p>I'm {name}</p>
+                <p>A 
+                  <span>
                     <TypeAnimation
                     sequence={[
                         'Enginner', // Types 'One'
@@ -93,35 +76,22 @@ function Home()  {
                         wrapper="span"
                         cursor={true}
                         repeat={Infinity}
-                        style={{display: 'inline-block' }}
-                    />
-                    </span>
-                    </p>
-                    <div className="d-flex gap-3 justify-content-center">
+                        style={{display: 'inline-block' }}/>
+                  </span>
+                </p>
 
-<Link to="/about">
-<button className="btn btn-primary"
-
->A propos</button>
-</Link>
-<Link to="/contact">
-<button className="btn btn-primary"
-
-
->Contact</button>
-</Link>
-                    </div>
-</div>
-                
-                
-
-                </div >
+                <div className="d-flex gap-3 justify-content-center">
+                    <Link to="/about">
+                      <button className="btn btn-primary">A propos</button>
+                    </Link>
+                    <Link to="/contact">
+                      <button className="btn btn-primary">Contact</button>
+                    </Link>
+                </div>
+              </div>
             </div>
-
-
-
-
-          </Container>
+          </div>
+        </div>
       </StyledAboutMe>
     </>
     )
